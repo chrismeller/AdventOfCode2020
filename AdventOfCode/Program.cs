db4074fb48648a8d0ddaea768d6f0d4c1b04a200
Chris Meller <chris@doesnthaveone.com>
2020-12-08 13:44:09 +0200

    Day 3 WIP

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -10,7 +10,9 @@ namespace AdventOfCode
             //var answer = Day1.Problem2.Solve();
 
             //var answer = Day2.Problem1.Solve();
-            var answer = Day2.Problem2.Solve();
+            //var answer = Day2.Problem2.Solve();
+
+            var answer = Day3.Problem1.Solve();
 
             Console.Out.WriteLine(answer);
         }
